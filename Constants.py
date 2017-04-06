b192ba0ae43267c40ab5e3221ec1f708eaa6c08e
moraw <krzymoraw@gmail.com>
2017-04-06 17:01:46 +0200

    Added some new constants

--- Constants.py
+++ Constants.py
@@ -20,8 +20,9 @@ pxWidth = 718.243e-12
 ewfLambda = 1.97e-12
 dfStepMin, dfStepMax, dfStepChange = 0.4, 1.2, 0.02
 ccWidgetDim = 1024
-# badPxThreshold = 1.1  # seria h1
-badPxThreshold = 2.3    # serie holo+-
+badPxThreshold = 4.0
+# badPxThreshold = 1.1    # seria h1
+# badPxThreshold = 2.3    # serie holo+-
 gridDim = 3
 nDivForUnwarp = 8
 
